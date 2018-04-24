1ebc0e5099de5c909bff4e20364ac6c989a23403
Rebecca Govert <becci.govert@gmail.com>
2018-04-24 16:11:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,5 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It\'s ${days} days until ${holiday}!`
 }
+
+/* tried (days, holidays) but it needed to be in order (holiday, days). Must watch this order of what directions are asking. I though I needed days first because days came first in my return line. Not true. */
